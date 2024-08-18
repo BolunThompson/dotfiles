edb49b653046f1a285d24cb656b56b204a059821
Bolun Thompson <Bolun Thompson>
2024-08-18 07:40:24 -0700

    add laptop hw config to repo

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
   imports =
     [
       # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./laptop-hw.nix
     ];
 
   systemd.services.NetworkManager-wait-online.enable = true;

5d6348343e92cee769877e9e9fc2dbd26437bb88
Bolun Thompson <me@bolun.dev>
2024-08-23 07:54:05 -0700

    add proper jupyter hash

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -2,10 +2,9 @@ c = get_config()
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 60001
 c.PasswordIdentityProvider.password_required = True
+c.PasswordIdentityProvider.hashed_password = "argon2:$argon2id$v=19$m=10240,t=10,p=8$1D2H/wKMvuydulAk3JKNzg$3hTBWN+bCR43sdNMvo1CHywMrg45dtHHefJKpeNIqMQ"
 
 c.NotebookApp.open_browser = False
 c.LabApp.open_browser = False
 c.ServerApp.open_browser = False
 c.ExtensionApp.open_browser = False
-
-c.LabApp.password = c.NotebookApp.password = "sha1:197b7fa43b5d83b3a15e89b5b4e893c4e8c91239"

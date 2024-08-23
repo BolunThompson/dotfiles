bc9dfa213000f374e58368a58358111812c25e96
Bolun Thompson <me@bolun.dev>
2024-08-22 18:08:57 -0700

    change jupyter config to not launch browser

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -3,3 +3,8 @@ c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 60001
 c.PasswordIdentityProvider.password_required = True
 c.PasswordIdentityProvider.password = 'Keilun 1228!'
+
+c.NotebookApp.open_browser = False
+c.LabApp.open_browser = False
+c.ServerApp.open_browser = False
+c.ExtensionApp.open_browser = False

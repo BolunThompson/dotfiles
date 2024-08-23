c147eacd3abefe3c3d07ceb3aab1fa27cdd5100a
Bolun Thompson <me@bolun.dev>
2024-08-22 20:25:36 -0700

    fix typo

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -2,9 +2,10 @@ c = get_config()
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 60001
 c.PasswordIdentityProvider.password_required = True
-c.PasswordIdentityProvider.password = 'Keilun 1228!'
 
 c.NotebookApp.open_browser = False
 c.LabApp.open_browser = False
 c.ServerApp.open_browser = False
 c.ExtensionApp.open_browser = False
+
+c.LabApp.password = c.NotebookApp.password = "8a51cf8eb235b1438deb9ccf2497fc49ee1dca1c"
